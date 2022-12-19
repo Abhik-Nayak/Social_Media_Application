63943122d8ff787ad5c7608ce3c43adf2accc177
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2022-12-19 15:50:34 +0530

    Abhik/19-12-2022

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextField,Button,Typography,Paper } from '@material-ui/core';
+import useStyles from "./styles";
+
+const Form = () => {
+    const classes= useStyles();
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
