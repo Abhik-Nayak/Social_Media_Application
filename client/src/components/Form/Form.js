98f0140585d57f58a506e409370c03a4c87f18fb
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-02-18 15:34:28 +0530

    Abhik//18-02-2023

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from "react-file-base64";
@@ -40,7 +41,7 @@ const Form = (currentId) => {
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostdata({ ...postData, creator: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostdata({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostdata({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostdata({ ...postData, tags: e.target.value })} />
+        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostdata({ ...postData, tags: e.target.value.split(",") })} />
         <div className={classes.fileInput}><FileBase type="file" multiple={false} value={postData.selectedFile} onDone={({ base64 }) => setPostdata({ ...postData, selectedFile: base64 })} /></div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
--- server/index.js
+++ server/index.js
@@ -8,16 +8,23 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 
-app.use(bodyParser.json({limit:"30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts',postRoutes);
-const CONNECTION_URL= 'mongodb+srv://abhiknayak:Abhik@1998@cluster0.8iiqxqw.mongodb.net/?retryWrites=true&w=majority'; 
-const PORT = process.env.PORT ||  5000 ;
+app.use('/posts', postRoutes);
+const CONNECTION_URL = 'mongodb+srv://abhiknayak:Abhik@1998@cluster0.8iiqxqw.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true })
-        .then(()=>app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
-        .catch((error)=> console.log(error.message));
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//         .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+//         .catch((error) => console.log(error.message));
+
+mongoose.connect("mongodb://localhost:27017/social_media", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        family: 4,
+}).then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+        .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
